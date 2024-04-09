af67745e55ae32302e369da40ed65de885593a22
blnche <peltierblanche@gmail.com>
2024-04-09 19:50:19 +0200

    done

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+const bodyParser = require ('body-parser');
+const multer = require ('multer');
+const upload = multer({dest: 'uploads/'});
 
 var app = express();
 
@@ -12,9 +15,33 @@ app.get('/', function (req, res) {
 });
 
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+
+  try {
+    const file = req.file;
+
+    if( !file ) {
+      res.json({
+        error: 'No file found'
+      });
+      return;
+    }
+
+    res.json({
+      name: file.originalname,
+      type: file.mimetype,
+      size: file.size
+    });
+
+  } catch (err) {
+    res.json({
+      error: err
+    });
+  }
+});
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
